cbd9d896ace39796b9ae86d73b2f6d91ae15a059
noahtizon <160686775+noahtizon@users.noreply.github.com>
2026-09-16 23:05:17 -0700

    Give program cards more reading time

--- data.js
+++ data.js
@@ -72,5 +72,5 @@ const CONFIG = {
     question2Entry: 'entry.494545951',
     question3Entry: 'entry.1056565161'
   },
-  quiz: { totalQuestions: 3, lessonDuration: 4000, revealDuration: 850 }
+  quiz: { totalQuestions: 3, lessonDuration: 6000, revealDuration: 850 }
 };
