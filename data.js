ca5ef0f035dbacf6b208a68f282041e66a67a919
noahtizon <160686775+noahtizon@users.noreply.github.com>
2026-09-16 18:48:58 -0700

    Map local photography to FUSION programs

--- data.js
+++ data.js
@@ -1,3 +1,25 @@
+const PHOTO_BASE = '/assets/photos/quizpics/';
+const programPhotos = {
+  'FUSION Projects': PHOTO_BASE + '1projects.png',
+  'Professional Development': PHOTO_BASE + 'prodev panel.JPG',
+  'Academic Programs': PHOTO_BASE + 'fusionconpresenty.png',
+  'Socials': PHOTO_BASE + 'karaoke social.png',
+  'FUSION Fams': PHOTO_BASE + 'fusion fam.JPG',
+  'FUSIONBook': PHOTO_BASE + 'pubsmedia workshop.png',
+  'Cultural Programming': PHOTO_BASE + 'tagalog word.png',
+  'FU-Fit & Intramurals': PHOTO_BASE + 'fusion fit.png',
+  'Alyansa': PHOTO_BASE + 'alyansapicnic.png',
+  'FUSION Mentorship': PHOTO_BASE + 'mentorship.png',
+  'Project Leadership': PHOTO_BASE + 'fusionconpresenty.png',
+  'FUSION Internship': PHOTO_BASE + 'intern class 1.png',
+  'Mentorship': PHOTO_BASE + 'mentorship.png',
+  'Publicity & Media': PHOTO_BASE + 'pubsmedia workshop.png',
+  'FUSIONCon': PHOTO_BASE + 'fusioncon.png',
+  'Alumni & Industry': PHOTO_BASE + 'fusioncon hosts.png',
+  'Study Hours': PHOTO_BASE + 'fusionconpresenty.png',
+  'FU-Fridays': PHOTO_BASE + 'karaoke social.png'
+};
+
 const questions = [
   {
     eyebrow: "START HERE",
